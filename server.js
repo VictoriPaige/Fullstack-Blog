9a691e60cd6a736aa6a745ff1d6df96011f32acc
Victoria Atkinson <105768093+VictoriPaige@users.noreply.github.com>
2022-08-30 14:11:47 -0400

    .put

--- server.js
+++ server.js
@@ -66,22 +66,22 @@ res.status(200).json(updateArticleInfo);
 // res.status(200).json(removeComment);
 
 
-// app.post('/api/articles/:name/remove-comments', (req,res)=>{
-//     const {username, text} = req.body;
-//     const articleName = req.params.name;
-//     const articleComment=req.params.comments.index;
-//    withDB(async(db)=>{
-// const articleInfo = await db.collection('articles').findOne({name:articleName});
-// await db.collection('articles').updateOne({comment:articleComment},{
-//     $unset:{
-//     comments:articleInfo.comments.remove({username,text}),
-//     },
-// });
-// const removeComment= await db.collection('articles').findOne({comment:articleComment});
-// res.status(200).json(removeComment);
+app.put('/api/articles/:name/comments', (req,res)=>{
+    const {username, text} = req.body;
+    const articleName = req.params.name;
+    const articleComment=req.params.comments;
+   withDB(async(db)=>{
+const articleInfo = await db.collection('articles').findOne({name:articleName});
+await db.collection('articles').updateOne({comment:articleComment},{
+    $unset:{
+    comments:articleInfo.comments.remove({username,text}),
+    },
+});
+const removeComment= await db.collection('articles').findOne({comment:articleComment});
+res.status(200).json(removeComment);
 
-// }, res);
-// });
+}, res);
+});
 
 // TEST ROUTE:
 // app.get('/', (req,res)=> res.send('Hello, World!'));
